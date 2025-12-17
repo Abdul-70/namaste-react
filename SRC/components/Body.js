31ca3fdc59c8f2adb0b7c865133ae0a0c13cdfe2
AbdurRahim999 <141570882+AbdurRahim999@users.noreply.github.com>
2025-12-17 11:45:59 +0530

    fetching server and addind shimmer UI

--- SRC/components/Body.js
+++ SRC/components/Body.js
@@ -1,10 +1,30 @@
 import RestaurentCard from "./RestaurentCard.js";
 import resList from "../utils/mockData.js";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer.js";
 
 const Body = () => {
 
-    const [RetaurentList, setRetaurentList] = useState(resList);
+    const [RetaurentList, setRetaurentList] = useState([]);
+
+    useEffect(()=>{
+        fetchData();
+    }, [])
+
+    const fetchData = async ()=>{
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6139&lng=77.2090");
+
+         const json = await data.json();
+        console.log(json);
+        
+         setRetaurentList(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+)
+    }
+
+   if(RetaurentList.length===0){
+    return <Shimmer/>
+   }
+
   return (
     <div className="body">
       <div className="filter">
